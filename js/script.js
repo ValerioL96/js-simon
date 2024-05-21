ff125eedc5567667cf4be9a13943cbc69036fb15
ValerioL96 <v.lembo1396@gmail.com>
2024-05-21 17:07:10 +0200

    Added timer 30 seconds

--- js/script.js
+++ js/script.js
@@ -12,3 +12,24 @@ while(numbersIa.length < 5){
      numbersIa.push(numbersRandomIA);
      console.log(numbersRandomIA)
 }
+
+//Da lì parte un timer di 30 secondi.
+
+//3) dopo aver stampato i 5 numri mi creo una funzione asincro che mi faccia un countdown di 30 secondi;
+
+
+let seconds = [];
+
+const timerSeconds = setInterval(function(){
+    seconds ++ ;
+    console.log(seconds);
+
+    if(seconds.length <= 30 ){
+        
+        clearInterval(timerSeconds);
+        alert('Finite time');
+    }
+}, 1000);
+
+//Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
